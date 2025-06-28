d0bfd1ec0b49fbd1dc3806dfdde9cfd785f7ba12
Christopher Pyter <christopherpyter@MacBookAir.attlocal.net>
2025-06-28 18:46:08 -0500

    Add getTopTags method for song tag analysis

--- src/main.py
+++ src/main.py
@@ -1,19 +1,24 @@
-# Import necessary modules
+# Imports enable API requests, formatted outputs, and retrieval of private credentials from 
+# separate environment
 import os
 from dotenv import load_dotenv
 import json
 import requests
 
-# Load environment variables from the .env file
 load_dotenv()
-
-# Defining API key
 api_key = os.getenv("Last_fm_API_Key")
 
-# Setting up parameters for test call
+# Testing a method call to identify potential errors with API key, overlooked parameters, etc.
 params = {'artist': 'Future', 'limit': 5, 'api_key': api_key, 'method': 'artist.getSimilar', 'format': 'json'}
+request = requests.get("http://ws.audioscrobbler.com/2.0", params=params)
+if request.status_code == 200:
+    data = request.json()
+#   print(json.dumps(data, indent=1))
+else:
+    print(f"Error: {request.status_code}")
 
-# Testing a method call
+# Using getTopTags to identify commonly-used tags for a song, helpful for mood analysis
+params = {'artist': 'John Lennon', 'track': 'Imagine', 'api_key': api_key, 'limit': 3, 'method': 'track.getTopTags', 'format': 'json'}
 request = requests.get("http://ws.audioscrobbler.com/2.0", params=params)
 if request.status_code == 200:
     data = request.json()
