af7e1242a576acbbfebaf40b4a2a9eff17de7701
Christopher Pyter <christopherpyter@MacBookAir.attlocal.net>
2025-06-29 22:53:08 -0500

    Added automation of song tag retrieval

--- src/main.py
+++ src/main.py
@@ -25,3 +25,17 @@ if request.status_code == 200:
     print(json.dumps(data, indent=1))
 else:
     print(f"Error: {request.status_code}")
+
+# Testing iteration through a list of songs to automate retrieval of tags for each song
+params = {'artist': 'Future', 'limit': 5, 'api_key': api_key, 'method': 'artist.getTopTags', 'format': 'json'}
+song_list = [{'artist': 'John Lennon', 'track': 'Imagine'}, {'artist': 'Mac Miller', 
+        'track': 'The Spins'}, {'artist': 'The Smashing Pumpkins', 'track': '1979'}]
+for song in song_list:
+    if request.status_code == 200:
+        params['artist'] = song['artist']
+        params['track'] = song['track']
+        request = requests.get("http://ws.audioscrobbler.com/2.0", params=params)
+        data = request.json()
+        print(json.dumps(data, indent=1))
+    else:  
+        print(f"Error: {request.status_code}")
