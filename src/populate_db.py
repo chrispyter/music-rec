31fa8702ea8bb72a78f85d6939d393430b82363a
Christopher Pyter <christopherpyter@Christophers-MacBook-Air-162.local>
2025-09-13 03:31:01 -0500

    Added and improved logic to ensure only valid songs with tags are processed

--- src/populate_db.py
+++ src/populate_db.py
@@ -1,9 +1,3 @@
-#FROM EACH TAG IN TAG LIST:
-#        FIND TOP TRACKS WITH THAT TAG
- #       FOR EACH TRACK ADDED TO DB:
-#            GET THE TOP 10 TAGS
- #           ADD TAGS TO TAG LIST
-
 import os
 from dotenv import load_dotenv
 import json
@@ -13,31 +7,62 @@ import sqlite3
 load_dotenv()
 api_key = os.getenv("Last_fm_API_Key")
 
-popular_genres = ['pop', 'hip-hop', 'hip hop', 'rap', 'rock', 'r&b', 'r and b', 'latin', 'edm', 'electronic',
-          'kpop', 'k-pop', 'korean pop', 'country', 'indie']
+#  CURRENTLY FIGURING OUT HOW TO CHECK SONG NAME AND ARTIST BEFORE INSERTING INTO DB
 
-# Retrieving 100 songs from each genre in list
-for genre in popular_genres:
-    params = {'tag': genre, 'limit': 100, 'api_key': api_key, 'method': 'tag.getTopTracks', 'format': 'json'}
-    request = requests.get("http://ws.audioscrobbler.com/2.0", params=params)
-    if request.status_code == 200:
-        data = request.json()
-        tracks = data['tracks']['track']
-        for t in tracks:
-            song_name = t['name']
-            artist = t['artist']['name']
-            print(f'Song: {song_name}, Artist: {artist}, Genre: {genre}')
-    else:
-        print(f"Error: {request.status_code}")
+# edge cases:
+# - song/artist isn't found (in loop alr)
+# - tags aren't found (in loop alr)
+# - remove duplicates if found (built-in with sql)
 
-# Connecting to SQL
-connection = sqlite3.connect('music_rec_schema')
-cursor = connection.cursor()
+# - incorporate a while loop with a counter so the loop stops when 100 songs are added
+#     - just incase it skips songs that don't have a name or tags 
 
 
+# def input_song()
+#     users_song = 
 
-# NEXT STEPS:
-# Clean up output
-# Make sure it's 100 songs for each genre
-# Integrate with SQL
-# Retrieve tags from songs added to database
+# popular_genres = ['pop', 'hip-hop', 'hip hop', 'rap', 'rock', 'r&b', 'r and b', 'latin', 'edm', 'electronic',
+#           'kpop', 'k-pop', 'korean pop', 'country', 'indie']
+
+# # Retrieving 100 songs from each genre in list
+# for genre in popular_genres:
+params = {'tag': 'rap', 'limit': 100, 'api_key': api_key, 'method': 'tag.getTopTracks', 'format': 'json'}
+request = requests.get("http://ws.audioscrobbler.com/2.0", params=params)
+if request.status_code == 200:
+    data = request.json()
+    tracks = data['tracks']['track']
+    for t in tracks:
+        song_name = t.get('name')
+        artist_name = t.get('artist', {}).get('name')
+        if song_name and artist_name:
+            params = {'track': song_name, 'artist': artist_name, 'api_key': api_key, 'method': 'track.getTopTags', 'format': 'json'}
+            request = requests.get("http://ws.audioscrobbler.com/2.0", params=params)
+        else:
+            continue
+        if request.status_code == 200:
+            data = request.json()
+            tags = data['toptags']['tag']
+            tags_list = []
+            for t in tags:
+                tag = t.get('name')
+                if tag:
+                    tags_list.append(tag)
+                    # OR would i not need a list. could it just add each one iteratively
+
+
+# # Connecting to SQL
+# connection = sqlite3.connect('music_rec_schema')
+# cursor = connection.cursor()
+
+# THIS IS FOR TESTING FUNCTION OUTPUT
+
+# params = {'tag': 'rock', 'limit': 100, 'api_key': api_key, 'method': 'tag.getTopTracks', 'format': 'json'}
+# request = requests.get("http://ws.audioscrobbler.com/2.0", params=params)
+# if request.status_code == 200:
+#     data = request.json()
+#     print(json.dumps(data, indent=1))
+# else:
+#     print(f"Error: {request.status_code}")
+
+# song name = name
+# artist = artist[name]
