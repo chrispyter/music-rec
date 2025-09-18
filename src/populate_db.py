d0a3466afc9377ec5e7765bd9fb9c46047deea9a
Christopher Pyter <christopherpyter@Christophers-MacBook-Air-162.local>
2025-09-18 01:03:19 -0500

    Fixed loop control logic and infinite loop caused by while loop

--- src/populate_db.py
+++ src/populate_db.py
@@ -14,44 +14,42 @@ api_key = os.getenv("Last_fm_API_Key")
 # - tags aren't found (in loop alr)
 # - remove duplicates if found (built-in with sql)
 
-# - incorporate a while loop with a counter so the loop stops when 100 songs are added
-#     - just incase it skips songs that don't have a name or tags 
-
-
 # def input_song()
 #     users_song = 
 
-# popular_genres = ['pop', 'hip-hop', 'hip hop', 'rap', 'rock', 'r&b', 'r and b', 'latin', 'edm', 'electronic',
-#           'kpop', 'k-pop', 'korean pop', 'country', 'indie']
-
-# # Retrieving 100 songs from each genre in list
-# for genre in popular_genres:
-params = {'tag': 'rap', 'limit': 100, 'api_key': api_key, 'method': 'tag.getTopTracks', 'format': 'json'}
-request = requests.get("http://ws.audioscrobbler.com/2.0", params=params)
-if request.status_code == 200:
-    data = request.json()
-    tracks = data['tracks']['track']
-    for t in tracks:
-        song_name = t.get('name')
-        artist_name = t.get('artist', {}).get('name')
-        if song_name and artist_name:
-            params = {'track': song_name, 'artist': artist_name, 'api_key': api_key, 'method': 'track.getTopTags', 'format': 'json'}
-            request = requests.get("http://ws.audioscrobbler.com/2.0", params=params)
-        else:
-            continue
-        if request.status_code == 200:
-            data = request.json()
-            tags = data['toptags']['tag']
-            tags_list = []
-            for t in tags:
-                tag = t.get('name')
-                if tag and len(tags_list) < 10:
-                    tag = tag.lower()
-                    tags_list.append(tag)
-                print(f'Song {count}:', song_name, artist_name, tags_list)
-                if not tags_list:
+popular_genres = ['pop', 'hip-hop', 'hip hop', 'rap', 'rock', 'r&b', 'r and b', 'latin', 'edm', 'electronic', 'kpop', 'k-pop', 'korean pop', 'country', 'indie']
+for genre in popular_genres:
+    count = 0
+    params = {'tag': genre, 'limit': 120, 'api_key': api_key, 'method': 'tag.getTopTracks', 'format': 'json'}
+    request = requests.get("http://ws.audioscrobbler.com/2.0", params=params)
+    if request.status_code == 200:
+        data = request.json()
+        tracks = data['tracks']['track']
+        for trk in tracks:
+            if count >= 100:
+                break
+            else:
+                song_name = trk.get('name')
+                artist_name = trk.get('artist', {}).get('name')
+                if song_name and artist_name:
+                    params = {'track': song_name, 'artist': artist_name, 'api_key': api_key, 'method': 'track.getTopTags', 'format': 'json'}
+                    request = requests.get("http://ws.audioscrobbler.com/2.0", params=params)
+                else:
                     continue
-                    
+                if request.status_code == 200:
+                    data = request.json()
+                    tags = data['toptags']['tag']
+                    tags_list = []
+                    for t in tags:
+                        tag = t.get('name')
+                        if tag and len(tags_list) < 10:
+                            tag = tag.lower()
+                            tags_list.append(tag)
+                    if not tags_list:
+                        continue
+                    count += 1
+                    print(f'Song {count}:', song_name, artist_name, tags_list)
+
 
  # limit number of tags to 10
 
