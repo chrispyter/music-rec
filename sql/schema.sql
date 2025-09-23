e9d274db2af3dce6b3e08955d23ca7d7c823cc60
Christopher Pyter <christopherpyter@Christophers-MacBook-Air-162.local>
2025-09-23 17:15:48 -0500

    Changed schema syntax to match SQLites

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,18 +1,17 @@
-CREATE DATABASE music_rec;
-USE music_rec;
-
 CREATE TABLE songs
 (song_name VARCHAR(75) NOT NULL,
 artist VARCHAR(75) NOT NULL,
-song_id INT PRIMARY KEY AUTO_INCREMENT);
+song_id INTEGER PRIMARY KEY AUTOINCREMENT,
+UNIQUE(song_name, artist));
 
 CREATE TABLE tags
 (tag VARCHAR(75) NOT NULL,
-tag_id INT PRIMARY KEY AUTO_INCREMENT);
+tag_id INTEGER PRIMARY KEY AUTOINCREMENT,
+UNIQUE(tag));
 
 CREATE TABLE song_tags
-(song_id INT,
-tag_id INT,
+(song_id INTEGER,
+tag_id INTEGER,
 FOREIGN KEY (song_id) REFERENCES songs(song_id),
 FOREIGN KEY (tag_id) REFERENCES tags(tag_id),
 PRIMARY KEY (song_id, tag_id));
